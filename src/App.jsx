6e78f8a8efd7f0b19ba23a9ee5f1ef45c3390cd2
Stking19 <okolistephen43@gmail.com>
2025-03-10 18:17:24 -0700

    fixed reviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import { HashRouter, Route, Routes  } from 'react-router-dom'
-// import LandingPage from './pages/Landing/LandingPage'
+import LandingPage from './pages/Landing/LandingPage'
 import Login from './pages/Auth/Login'
 import SignUp from './pages/Auth/SignUp'
 import ForgotPassword from './pages/Auth/ForgotPassword'
 import Resetpassword from './pages/Auth/Resetpassword'
-import FundWallet from './pages/Stephen/fundwallet/FundWallet'
-import Payment from './pages/Stephen/payment/Payment'
 
 const App = () => {
   return (
     <HashRouter>
         <Routes>
-          <Route path="/" element={<FundWallet />}/>
+          <Route path="/" element={<LandingPage />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/signup" element={<SignUp />}/>
           <Route path="/forgot-password" element={<ForgotPassword />}/>
           <Route path="/reset-password" element={<Resetpassword />}/>
-          <Route path="/user/deposit/manual" element={<Payment />}/>
         </Routes>
     </HashRouter>
   )

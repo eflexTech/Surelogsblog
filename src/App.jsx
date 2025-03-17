cfde3b52a5d117c86ba0939a1fe715a2a99a78e0
Stking19 <okolistephen43@gmail.com>
2025-03-17 06:29:23 -0700

    finished rules and terms, edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
-// import LandingPage from "./pages/Landing/LandingPage";
+import LandingPage from "./pages/Landing/LandingPage";
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
 import ForgotPassword from "./pages/Auth/ForgotPassword";
 import Resetpassword from "./pages/Auth/Resetpassword";
-import TermsOfUse from "./pages/Stephen/terms-of-use/TermsOfUse";
-// import Rules from "./pages/Stephen/rules/Rules";
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<TermsOfUse />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
